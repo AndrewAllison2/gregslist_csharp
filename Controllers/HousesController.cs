24d2fe1a6b40401cf7ebbd426d99b1e1b9d20916
Andrew Allison <andrewallison5211@gmail.com>
2023-08-15 14:36:55 -0600

    get all houses

--- Controllers/HousesController.cs
+++ Controllers/HousesController.cs
@@ -6,5 +6,24 @@ namespace gregslist_csharp.Controllers;
     [Route("api/[controller]")]
     public class HousesController : ControllerBase
     {
-        
+    private readonly HousesService _housesService;
+
+    public HousesController(HousesService housesService)
+    {
+        _housesService = housesService;
     }
+
+    [HttpGet]
+    public ActionResult<List<House>> GetHouses()
+    {
+        try
+        {
+            List<House> houses = _housesService.GetHouses();
+            return Ok(houses);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
--- Repositories/HousesRepository.cs
+++ Repositories/HousesRepository.cs
@@ -7,6 +7,18 @@ namespace gregslist_csharp.Repositories
 {
     public class HousesRepository
     {
-        
+        private readonly IDbConnection _db;
+
+        public HousesRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        internal List<House> GetHouses()
+        {
+            string sql = "SELECT * FROM houses;";
+            List<House> houses = _db.Query<House>(sql).ToList();
+            return houses;
+        }
     }
 }
